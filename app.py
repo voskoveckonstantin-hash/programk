faebe2b326d9156ffae0ac139083611ce3eba7a9
П-24-68Б <@Polytech2.kz>
2026-04-27 08:47:03 +0500

    Начальная версия проекта

---
+++ app.py
@@ -0,0 +1,13 @@
+from utils import add, subtract, multiply, divide
+
+
+def main():
+    print("Простой калькулятор")
+    print("2 + 3 =", add(2, 3))
+    print("10 - 4 =", subtract(10, 4))
+    print("5 * 6 =", multiply(5, 6))
+    print("8 / 2 =", divide(8, 2))
+
+
+if __name__ == "__main__":
+    main()
